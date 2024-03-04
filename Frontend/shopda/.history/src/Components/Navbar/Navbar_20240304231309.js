7a1abbbec0e1c6c94cacb96f532d07287f6ee5b9
Gnut93 <tungddps25683@fpt.edu.vn>
2024-03-04 23:15:02 +0700

    update

---
+++ Frontend/shopda/.history/src/Components/Navbar/Navbar_20240304231309.js
@@ -0,0 +1,32 @@
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <div>
+      <div className="phone">
+        <span>HOTLINE : 0901.379.586</span>
+      </div>
+      <nav>
+        <div className="logo">
+          <a>LOGO</a>
+        </div>
+        <div>
+          <ul>
+            <li>TRANG CHỦ</li>
+            <li>SẢN PHẨM</li>
+            <li>BỘ SƯU TẬP</li>
+            <li>BLOGS</li>
+            <li>KHUYẾN MÃI</li>
+            <li>ORDER THEO SỞ THÍCH</li>
+            <li>TIN TỨC</li>
+          </ul>
+        </div>
+        <div className="icon">
+          <i className="fas fa-search"></i>
+          <i className="fas fa-shopping-cart"></i>
+          <span>(0)</span>
+        </div>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
