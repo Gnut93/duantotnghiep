357593cb4faee42d55687552831191ea73a9e2bf
phatnguyen1235 <phatnguyen.26092001@gmail.com>
2024-03-05 21:36:43 +0700

    footer

--- Frontend/shopda/src/Components/Footer/Footer.js
+++ Frontend/shopda/src/Components/Footer/Footer.js
@@ -3,48 +3,52 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="community">
-        <div className="b-title">Tham gia cộng đồng #DACommunity</div>
-        {/* Phần cộng đồng */}
-        <input
-          type="email"
-          placeholder="Email của bạn..."
-        />
-        <button>→</button>
-        <div className="title">Đồng hành cùng chúng tôi</div>
-        {/* Các biểu tượng mạng xã hội */}
-        <ul className="social-icons">
-          <li>
-            <a href="/">
-              <i className="fas fa-facebook"></i>
-            </a>
-          </li>
-        </ul>
-        {/* ... */}
-      </div>
-
-      <div className="contact">
-        {/* Thông tin liên hệ */}
-        <p>CÔNG TY TNHH DA VIỆT NAM</p>
-        {/* ... */}
-      </div>
-
-      <div className="about">
-        {/* Các liên kết về chúng tôi */}
-        {/* ... */}
-      </div>
-
-      <div className="customerService">
-        {/* Các liên kết dịch vụ khách hàng */}
-        {/* ... */}
-      </div>
-
-      <div className="paymentAndShipping">
-        {/* Biểu tượng phương thức thanh toán và vận chuyển */}
-        {/* ... */}
-      </div>
-    </div>
+    <footer className="footer">
+  	 <div className="container">
+  	 	<div className="row">
+  	 		<div className="footer-col">
+  	 			<h4>Về chúng tôi</h4>
+  	 			<ul>
+  	 				<li><a href="#">Giới thiệu</a></li>
+  	 				<li><a href="#">Dịch vụ</a></li>
+  	 				<li><a href="#">Liên hệ</a></li>
+  	 				<li><a href="#">Tuyển dụng</a></li>
+           
+  	 			</ul>
+  	 		</div>
+  	 		<div className="footer-col">
+  	 			<h4>Dịch vụ khách hàng</h4>
+  	 			<ul>
+  	 				<li><a href="#">Hướng dẫn mua hàng</a></li>
+  	 				<li><a href="#">Hướng dẫn đổi trả hàng</a></li>
+  	 				<li><a href="#">Chính sách đổi hàng và đổi </a></li>
+  	 				<li><a href="#">Chính sách vận chuyển</a></li>
+  	 				<li><a href="#">Câu hỏi thường gặp</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div className="footer-col">
+  	 			<h4>online shop</h4>
+  	 			<ul>
+  	 				<li><a href="#">Túi xách</a></li>
+  	 				<li><a href="#">Túi du lịch</a></li>
+  	 				<li><a href="#">Balo</a></li>
+  	 				<li><a href="#">Bóp-Ví</a></li>
+             <li><a href="#">Dây nịch</a></li>
+             <li><a href="#">Phụ kiện khác</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div className="footer-col">
+  	 			<h4>Theo dõi chúng tôi</h4>
+  	 			<div className="social-links">
+  	 				<a href="#"><i className="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i className="fab fa-twitter"></i></a>
+  	 				<a href="#"><i className="fab fa-instagram"></i></a>
+  	 				<a href="#"><i className="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
   );
 };
 
