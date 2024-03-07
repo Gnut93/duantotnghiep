fc642d31a259983c6fe33c237675e9cabcc54be4
Ta Toan <tttoan1801@gmail.com>
2024-03-07 09:13:11 +0700

    fix shop

--- Frontend/shopda/src/Components/Shop/Shop.js
+++ Frontend/shopda/src/Components/Shop/Shop.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Navbar from '../Navbar/Navbar';
 import './Shop.css';
 
 const Shop = () => {
   return (
     <>
       <div className="container">
-        <div className="navbar">
-          <Navbar></Navbar>
-        </div>
         <div className="category">
           <div>
             {' '}
