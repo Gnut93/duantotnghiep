fb1871e3db754dd54d56df9143092bcfb52bd00e
NguyÃáÃÅnông Quý <nguyencongquy1304@gmail.com>
2024-03-08 21:04:41 +0700

    them duong dan

--- Frontend/shopda/src/admin/App.js
+++ Frontend/shopda/src/admin/App.js
@@ -1,25 +1,27 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Components/Home/Home';
-import Sidebar from './Components/Sidebar/Sidebar';
-import Navbar from './Components/Navbar/Navbar';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Navbar from "./Components/Navbar/Navbar";
 
 function AppAdmin() {
-  return (
-    <div className="wapper">
-      <Sidebar />
-      <Navbar />
-      <main className="main">
-        <Routes>
-          <Route
-            path="/"
-            element={<Home />}
-          />
-          {/* Thêm các Route khác dành riêng cho phần quản trị ở đây */}
-        </Routes>
-      </main>
-    </div>
-  );
+    return (
+        <div className="wapper">
+            <Sidebar />
+            <Navbar />
+            <main className="main">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/admin/loaihang" element={<Home />} />
+                    <Route path="/admin/donhang" element={<Home />} />
+                    <Route path="/admin/nguoidung" element={<Home />} />
+                    <Route path="/admin/khohang" element={<Home />} />
+                    <Route path="/admin/nhaplieu" element={<Home />} />
+                    {/* Thêm các Route khác dành riêng cho phần quản trị ở đây */}
+                </Routes>
+            </main>
+        </div>
+    );
 }
 
 export default AppAdmin;
--- Frontend/shopda/src/admin/Components/Sidebar/Sidebar.js
+++ Frontend/shopda/src/admin/Components/Sidebar/Sidebar.js
@@ -1,70 +1,74 @@
-import React from 'react';
-import './Sildebar.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Sildebar.css";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  return (
-    <section className="sidebar">
-      <Link to={'/'}>
-        <h4 className="brand">D/A Admin</h4>
-      </Link>
-      <ul className="side-menu top">
-        <li className="toggle-nav active">
-          <Link to={'/'}>
-            <span>
-              <i className="bx bxs-dashboard"></i>
-              <span className="text">Tổng quan</span>
-            </span>
-          </Link>
-        </li>
-        <li className="toggle-nav">
-          <Link to={'/shop'}>
-            <span>
-              <i className="bx bxs-shopping-bag-alt"></i>
-              <span className="text">Kho hàng</span>
-            </span>
-          </Link>
-        </li>
-        <li className="toggle-nav">
-          <Link to={'/loai'}>
-            <span>
-              <i className="bx bxs-doughnut-chart"></i>
-              <span className="text">Loại hàng</span>
-            </span>
-          </Link>
-        </li>
-        <li className="toggle-nav">
-          <span>
-            <i className="bx bxs-add-to-queue"></i>
-            <span className="text">Đơn hàng</span>
-          </span>
-        </li>
-        <li className="toggle-nav">
-          <span>
-            <i className="bx bxs-group"></i>
-            <span className="text">Người dùng</span>
-          </span>
-        </li>
-        <li className="toggle-nav">
-          <Link to={'/nhaplieu'}>
-            <span>
-              <i class="bx bxs-cylinder"></i>
-              <span className="text">Nhập liệu</span>
-            </span>
-          </Link>
-        </li>
-      </ul>
-      <span className="profile"></span>
-      <ul className="side-menu">
-        <li className="toggle-nav">
-          <span className="logout">
-            <i className="bx bxs-log-out-circle"></i>
-            <span className="text">Logout</span>
-          </span>
-        </li>
-      </ul>
-    </section>
-  );
+    return (
+        <section className="sidebar">
+            <Link to={"/admin"}>
+                <h4 className="brand">D/A Admin</h4>
+            </Link>
+            <ul className="side-menu top">
+                <li className="toggle-nav active">
+                    <Link to={"/admin"}>
+                        <span>
+                            <i className="bx bxs-dashboard"></i>
+                            <span className="text">Tổng quan</span>
+                        </span>
+                    </Link>
+                </li>
+                <li className="toggle-nav">
+                    <Link to={"/admin/khohang"}>
+                        <span>
+                            <i className="bx bxs-shopping-bag-alt"></i>
+                            <span className="text">Kho hàng</span>
+                        </span>
+                    </Link>
+                </li>
+                <li className="toggle-nav">
+                    <Link to={"/admin/loaihang"}>
+                        <span>
+                            <i className="bx bxs-doughnut-chart"></i>
+                            <span className="text">Loại hàng</span>
+                        </span>
+                    </Link>
+                </li>
+                <li className="toggle-nav">
+                    <Link to={"/admin/donhang"}>
+                        <span>
+                            <i className="bx bxs-add-to-queue"></i>
+                            <span className="text">Đơn hàng</span>
+                        </span>
+                    </Link>
+                </li>
+                <li className="toggle-nav">
+                    <Link to={"/admin/nguoidung"}>
+                        <span>
+                            <i className="bx bxs-add-to-queue"></i>
+                            <span className="text">Người Dùng</span>
+                        </span>
+                    </Link>
+                </li>
+                <li className="toggle-nav">
+                    <Link to={"/admin/nhaplieu"}>
+                        <span>
+                            <i class="bx bxs-cylinder"></i>
+                            <span className="text">Nhập liệu</span>
+                        </span>
+                    </Link>
+                </li>
+            </ul>
+            <span className="profile"></span>
+            <ul className="side-menu">
+                <li className="toggle-nav">
+                    <span className="logout">
+                        <i className="bx bxs-log-out-circle"></i>
+                        <span className="text">Logout</span>
+                    </span>
+                </li>
+            </ul>
+        </section>
+    );
 };
 
 export default Sidebar;
