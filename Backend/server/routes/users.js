f6274defd65243c6b2b5177b92888662f5d76a16
hoanls18302 <hoanlsps25637@fpt.edu.vn>
2024-03-08 18:51:57 +0700

    lam them api lich su don hang

--- Backend/server/routes/users.js
+++ Backend/server/routes/users.js
@@ -94,4 +94,43 @@ getUserInfo = async (email) => {
   });
 }
 
+
+
+
+//Lịch sử mua hàng
+router.get('/history/:id', (req, res) => {
+  var id = parseInt(req.params.id);
+  if (isNaN(id) || id < 1) {
+    res.json({ error: "ID không hợp lệ" });
+    return;
+  }
+
+  var sql = `SELECT * FROM bill where id_user = '${id}'`;
+  db.query(sql, (err, result) => {
+    if(err) {
+      res.json({ error: "Khong tim thay lich su mua hang" });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+//Chi tiết 1 đơn hàng
+router.get('/bill/:id', (req, res) => {
+  var id = parseInt(req.params.id);
+  if (isNaN(id) || id < 1) {
+    res.json({ error: "ID không hợp lệ" });
+    return;
+  }
+
+  var sql = `SELECT * FROM bill_detail where id_bill = '${id}'`;
+  db.query(sql, (err, result) => {
+    if(err) {
+      res.json({ error: "Khong tim thay chi tiet don hang" });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
 module.exports = router;
