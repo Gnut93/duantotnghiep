1e8471d3fd77c66ccec4bc7da26141120e990399
Ta Toan <tttoan1801@gmail.com>
2024-03-15 22:50:47 +0700

    about

--- Frontend/shopda/src/user/App.js
+++ Frontend/shopda/src/user/App.js
@@ -1,16 +1,18 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Home from "./Components/Home/Home";
-import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
-import Shop from "./Components/Shop/Shop";
-import Products from "./Components/Products/Products";
-import ProductDetail from "./Components/ProductDetail/ProductDetail";
-import ScrollToTop from "./Components/ScrollToTop/ScrollToTop";
-import ShowCar from "./Components/ShowCart/ShowCart";
-import Contact from "./Components/Contact/Contact";
-import Checkout from "./Components/Checkout/Checkout";
-import Search from "./Components/Search/Search";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Shop from './Components/Shop/Shop';
+import Products from './Components/Products/Products';
+import ProductDetail from './Components/ProductDetail/ProductDetail';
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
+import ShowCar from './Components/ShowCart/ShowCart';
+import Contact from './Components/Contact/Contact';
+import Checkout from './Components/Checkout/Checkout';
+import Search from './Components/Search/Search';
+import About from './Components/About/About';
+import Blog from './Components/Blog/Blog';
 function App() {
   return (
     <div className="wapper">
@@ -18,14 +20,56 @@ function App() {
       <ScrollToTop></ScrollToTop>
       <main className="main">
         <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/shop" exact element={<Shop />} />
-          <Route path="/cate/:id_cate" exact element={<Products />} />
-          <Route path="/search" exact element={<Search />} />
-          <Route path="/showcart" exact element={<ShowCar />} />
-          <Route path="/product/:id_pd" exact element={<ProductDetail />} />
-          <Route path="/contact" exact element={<Contact />} />
-          <Route path="/checkout" exact element={<Checkout />} />
+          <Route
+            path="/"
+            exact
+            element={<Home />}
+          />
+          <Route
+            path="/shop"
+            exact
+            element={<Shop />}
+          />
+          <Route
+            path="/cate/:id_cate"
+            exact
+            element={<Products />}
+          />
+          <Route
+            path="/search"
+            exact
+            element={<Search />}
+          />
+          <Route
+            path="/showcart"
+            exact
+            element={<ShowCar />}
+          />
+          <Route
+            path="/product/:id_pd"
+            exact
+            element={<ProductDetail />}
+          />
+          <Route
+            path="/contact"
+            exact
+            element={<Contact />}
+          />
+          <Route
+            path="/about"
+            exact
+            element={<About />}
+          />
+          <Route
+            path="/blogs"
+            exact
+            element={<Blog />}
+          />
+          <Route
+            path="/checkout"
+            exact
+            element={<Checkout />}
+          />
         </Routes>
       </main>
       <Footer></Footer>
---
+++ Frontend/shopda/src/user/Components/Blog/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Blog = () => {
+  return (
+    <section className="blog">
+      <Navbar></Navbar>
+    </section>
+  );
+};
+
+export default Blog;
