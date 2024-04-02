35846d39fb508d33262126e81815433d03766882
NguyÃáÃÅnông Quý <nguyencongquy1304@gmail.com>
2024-04-02 13:26:20 +0700

    thêm gifcode checkout

--- Backend/server/routes/admin-giftcode.js
+++ Backend/server/routes/admin-giftcode.js
@@ -1,87 +1,125 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var db = require('../models/database');
+var db = require("../models/database");
 
 async function queryDB(sql, params) {
-  return new Promise((resolve, reject) => {
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
+    return new Promise((resolve, reject) => {
+        db.query(sql, params, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
     });
-  });
 }
-router.get('/list', (req, res) => {
-  var sql = `SELECT * FROM gif_code`;
-  db.query(sql, (err, result) => {
-    if (err) {
-      res.json({ error: 'Khong tim thay danh muc' });
-    } else {
-      res.json(result);
-    }
-  });
+router.get("/list", (req, res) => {
+    var sql = `SELECT * FROM gif_code`;
+    db.query(sql, (err, result) => {
+        if (err) {
+            res.json({ error: "Khong tim thay danh muc" });
+        } else {
+            res.json(result);
+        }
+    });
 });
 
-router.get('/detail/:id', (req, res) => {
-  var id = parseInt(req.params.id);
-  if (isNaN(id) || id < 1) {
-    res.json({ error: 'ID không hợp lệ' });
-    return;
-  }
-  var sql = `SELECT * FROM gif_code WHERE id_gc=?`;
-  db.query(sql, [id], (err, result) => {
-    if (err) {
-      res.json({ error: 'Khong tim thay giftcode' });
-    } else {
-      res.json(result[0]);
+router.get("/detail/:id", (req, res) => {
+    var id = parseInt(req.params.id);
+    if (isNaN(id) || id < 1) {
+        res.json({ error: "ID không hợp lệ" });
+        return;
     }
-  });
+    var sql = `SELECT * FROM gif_code WHERE id_gc=?`;
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            res.json({ error: "Khong tim thay giftcode" });
+        } else {
+            res.json(result[0]);
+        }
+    });
 });
 
 //Thêm giftcode
-router.post('/add', async (req, res) => {
-  try {
-    var { code, quantity, price, expiration_date } = req.body;
-    var sql = `INSERT INTO gif_code (code, quantity, price, expiration_date) VALUES (?, ?, ?, ?)`;
-    await queryDB(sql, [code, quantity, price, expiration_date]);
-    res.json({ success: 'Thêm giftcode thành công' });
-  } catch (err) {
-    res.json({ error: err.message });
-  }
+router.post("/add", async (req, res) => {
+    try {
+        var { code, quantity, price, expiration_date } = req.body;
+        var sql = `INSERT INTO gif_code (code, quantity, price, expiration_date) VALUES (?, ?, ?, ?)`;
+        await queryDB(sql, [code, quantity, price, expiration_date]);
+        res.json({ success: "Thêm giftcode thành công" });
+    } catch (err) {
+        res.json({ error: err.message });
+    }
 });
 
 //Sửa thông tin giftcode
-router.put('/edit/:id', async (req, res) => {
-  var id = parseInt(req.params.id);
-  if (isNaN(id) || id < 1) {
-    res.json({ error: 'ID không hợp lệ' });
-    return;
-  }
-  try {
-    var { code, quantity, price, expiration_date } = req.body;
-    var sql = `UPDATE gif_code SET code=?, quantity=?, price=?, expiration_date=? WHERE id_gc=?`;
-    await queryDB(sql, [code, quantity, price, expiration_date, id]);
-    res.json({ success: 'Sửa giftcode thành công' });
-  } catch (err) {
-    res.json({ error: err.message });
-  }
+router.put("/edit/:id", async (req, res) => {
+    var id = parseInt(req.params.id);
+    if (isNaN(id) || id < 1) {
+        res.json({ error: "ID không hợp lệ" });
+        return;
+    }
+    try {
+        var { code, quantity, price, expiration_date } = req.body;
+        var sql = `UPDATE gif_code SET code=?, quantity=?, price=?, expiration_date=? WHERE id_gc=?`;
+        await queryDB(sql, [code, quantity, price, expiration_date, id]);
+        res.json({ success: "Sửa giftcode thành công" });
+    } catch (err) {
+        res.json({ error: err.message });
+    }
+});
+// cập nhật số lượng sản phẩm
+router.put("/edit-quantity/:id", async (req, res) => {
+    var id_gc = parseInt(req.params.id);
+    if (isNaN(id_gc) || id_gc < 1) {
+        res.json({ error: "ID không hợp lệ" });
+        return;
+    }
+    try {
+        const { quantity } = req.body;
+        console.log(quantity);
+        const sql = `UPDATE gif_code SET  quantity='${quantity}' WHERE id_gc='${id_gc}'`;
+        await queryDB(sql);
+        res.json({ success: "Cập nhật số lượng  sản phẩm thành công" });
+    } catch (err) {
+        res.json({ error: err.message });
+    }
 });
 
 //Xóa giftcode
-router.delete('/delete/:id', async (req, res) => {
-  var id = parseInt(req.params.id);
-  if (isNaN(id) || id < 1) {
-    res.json({ error: 'ID không hợp lệ' });
-    return;
-  }
-  try {
-    await queryDB(`DELETE FROM gif_code WHERE id_gc=?`, [id]);
-    res.json({ success: 'Xóa giftcode thành công' });
-  } catch (err) {
-    res.json({ error: err.message });
-  }
+router.delete("/delete/:id", async (req, res) => {
+    var id = parseInt(req.params.id);
+    if (isNaN(id) || id < 1) {
+        res.json({ error: "ID không hợp lệ" });
+        return;
+    }
+    try {
+        await queryDB(`DELETE FROM gif_code WHERE id_gc=?`, [id]);
+        res.json({ success: "Xóa giftcode thành công" });
+    } catch (err) {
+        res.json({ error: err.message });
+    }
+});
+// Route để kiểm tra mã giảm giá và trả về giá giảm
+router.post("/check-discount", (req, res) => {
+    const { discount } = req.body;
+
+    const sql = `SELECT id_gc, price, quantity FROM gif_code WHERE code = ?`;
+    db.query(sql, [discount], (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        if (result.length > 0) {
+            const { id_gc, price, quantity } = result[0];
+            res.send({ validDiscount: true, id_gc, price, quantity });
+        } else {
+            res.send({
+                validDiscount: false,
+                error: "Mã giảm giá không tồn tại.",
+            });
+        }
+    });
 });
 
 module.exports = router;
