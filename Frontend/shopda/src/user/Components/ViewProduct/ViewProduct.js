ce441e42fe2b279a28da7250ade30b60bb12b7fe
NguyÃáÃÅnông Quý <nguyencongquy1304@gmail.com>
2024-04-04 11:19:01 +0700

    làm favorite sản phẩm

--- Frontend/shopda/src/user/Components/ViewProduct/ViewProduct.js
+++ Frontend/shopda/src/user/Components/ViewProduct/ViewProduct.js
@@ -1,168 +1,221 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import './ViewProduct.css';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { themSP } from '../cartSlice';
+import React, { useCallback, useEffect, useState } from "react";
+import "./ViewProduct.css";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { themSP } from "../cartSlice";
 
 const ViewProduct = () => {
-  const [products, setProducts] = useState([]);
-  const [colors, setColors] = useState({});
-  const [selectedColorIds, setSelectedColorIds] = useState({});
-  const cart = useSelector((state) => state.cart.listSP);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    fetch(`http://localhost:4000/products/list/view/8`)
-      .then((res) => res.json())
-      .then((data) => {
-        setProducts(data);
-        const initialColorIds = {};
-        const colorPromises = data.map((product) =>
-          fetch(`http://localhost:4000/products/color/${product.id_pd}`)
+    const [products, setProducts] = useState([]);
+    const [colors, setColors] = useState({});
+    const [selectedColorIds, setSelectedColorIds] = useState({});
+    const cart = useSelector((state) => state.cart.listSP);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        fetch(`http://localhost:4000/products/list/view/8`)
             .then((res) => res.json())
-            .then((colorData) => {
-              setColors((prevColors) => ({
-                ...prevColors,
-                [product.id_pd]: colorData.map((color) => ({
-                  ...color,
-                  maxQuantity: color.quantity,
-                })),
-              }));
-              if (colorData.length > 0) {
-                initialColorIds[product.id_pd] = colorData[0].id_color;
-              }
-            })
+            .then((data) => {
+                setProducts(data);
+                const initialColorIds = {};
+                const colorPromises = data.map((product) =>
+                    fetch(
+                        `http://localhost:4000/products/color/${product.id_pd}`
+                    )
+                        .then((res) => res.json())
+                        .then((colorData) => {
+                            setColors((prevColors) => ({
+                                ...prevColors,
+                                [product.id_pd]: colorData.map((color) => ({
+                                    ...color,
+                                    maxQuantity: color.quantity,
+                                })),
+                            }));
+                            if (colorData.length > 0) {
+                                initialColorIds[product.id_pd] =
+                                    colorData[0].id_color;
+                            }
+                        })
+                );
+
+                Promise.all(colorPromises).then(() => {
+                    setSelectedColorIds(initialColorIds);
+                });
+            });
+    }, []);
+
+    const handleAddToCart = (product) => {
+        const id_color = selectedColorIds[product.id_pd];
+        const nameColor = colors[product.id_pd].find(
+            (color) => color.id_color === id_color
+        ).name;
+        const maxQuantity = colors[product.id_pd].find(
+            (color) => color.id_color === id_color
+        ).quantity;
+        const colorInfo = colors[product.id_pd].find(
+            (color) => color.id_color === id_color
         );
 
-        Promise.all(colorPromises).then(() => {
-          setSelectedColorIds(initialColorIds);
-        });
-      });
-  }, []);
+        if (!colorInfo) {
+            alert("Màu sắc không hợp lệ.");
+            return;
+        }
 
-  const handleAddToCart = (product) => {
-    const id_color = selectedColorIds[product.id_pd];
-    const nameColor = colors[product.id_pd].find(
-      (color) => color.id_color === id_color
-    ).name;
-    const maxQuantity = colors[product.id_pd].find(
-      (color) => color.id_color === id_color
-    ).quantity;
-    const colorInfo = colors[product.id_pd].find(
-      (color) => color.id_color === id_color
-    );
+        const currentProductInCart = cart.find(
+            (p) => p.id_pd === product.id_pd && p.id_color === id_color
+        );
+        if (
+            currentProductInCart &&
+            currentProductInCart.soluong + 1 > colorInfo.maxQuantity
+        ) {
+            alert(`Số lượng tối đa cho màu này là ${colorInfo.maxQuantity}.`);
+        } else {
+            // Logic để thêm sản phẩm vào giỏ hàng hoặc tăng số lượng
+            dispatch(
+                themSP({
+                    ...product,
+                    id_color,
+                    nameColor,
+                    maxQuantity,
+                    soluong: currentProductInCart
+                        ? currentProductInCart.soluong + 1
+                        : 1,
+                })
+            );
+        }
+    };
+    const selectColor = useCallback((productId, colorId) => {
+        setSelectedColorIds((prev) => ({
+            ...prev,
+            [productId]: colorId,
+        }));
+    }, []);
 
-    if (!colorInfo) {
-      alert('Màu sắc không hợp lệ.');
-      return;
-    }
-
-    const currentProductInCart = cart.find(
-      (p) => p.id_pd === product.id_pd && p.id_color === id_color
-    );
-    if (
-      currentProductInCart &&
-      currentProductInCart.soluong + 1 > colorInfo.maxQuantity
-    ) {
-      alert(`Số lượng tối đa cho màu này là ${colorInfo.maxQuantity}.`);
-    } else {
-      // Logic để thêm sản phẩm vào giỏ hàng hoặc tăng số lượng
-      dispatch(
-        themSP({
-          ...product,
-          id_color,
-          nameColor,
-          maxQuantity,
-          soluong: currentProductInCart ? currentProductInCart.soluong + 1 : 1,
-        })
-      );
-    }
-  };
-  const selectColor = useCallback((productId, colorId) => {
-    setSelectedColorIds((prev) => ({
-      ...prev,
-      [productId]: colorId,
-    }));
-  }, []);
-
-  return (
-    <section className="view">
-      <div className="container">
-        <h3 className="related-heading">Sản Phẩm Được Xem Nhiều</h3>
-        <div className="related-product">
-          {products.map((item, i) => (
-            <div
-              className="products-item"
-              key={i}>
-              <div className="products-main">
-                <div className="products-main-content">
-                  <img
-                    className="products-image"
-                    src={item.image}
-                    alt={item.name}
-                  />
-                  <div className="products-content">
-                    <ul className="products-social">
-                      <li className="products-social-item">
-                        <Link
-                          to={`/product/${item.id_pd}`}
-                          className="products-social-link">
-                          <i className="fas fa-search"></i>
-                        </Link>
-                      </li>
-                      <li className="products-social-item">
-                        <span
-                          className="products-social-link cart"
-                          onClick={() => handleAddToCart(item)}>
-                          <i className="fas fa-cart-plus"></i>
-                        </span>
-                      </li>
-                      <li className="products-social-item">
-                        <span className="products-social-link">
-                          <i className="fas fa-heart"></i>
-                        </span>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="products-info">
-                  <h4 className="products-name">{item.name}</h4>
-                  <div className="products-color-list">
-                    {colors[item.id_pd]?.map((color) => (
-                      <div
-                        key={color.id_color}
-                        className={`products-color-item ${
-                          selectedColorIds[item.id_pd] === color.id_color
-                            ? 'active'
-                            : ''
-                        }`}
-                        style={{ backgroundColor: color.code }}
-                        onClick={() =>
-                          selectColor(item.id_pd, color.id_color)
-                        }></div>
+    const user = useSelector((state) => state.auth.user);
+    const checkLogin = useSelector((state) => state.auth.daDangNhap);
+    const idUser = user ? user.id_user : null;
+    const HandleOnClickFavorite = async (item) => {
+        if (checkLogin === true) {
+            const id_pd = item.id_pd;
+            const id_user = idUser;
+            console.log(id_pd, id_user);
+            try {
+                const url = "http://localhost:4000/favorite/add";
+                const opt = {
+                    method: "post",
+                    body: JSON.stringify({ id_pd: id_pd, id_user: id_user }),
+                    headers: { "Content-Type": "application/json" },
+                };
+                const res = await fetch(url, opt);
+                const responseData = await res.json();
+                alert("Đã thêm vào danh sách yêu thích", responseData);
+            } catch (error) {
+                console.error("Lỗi khi thêm yêu thích sản phẩm: ", error);
+            }
+        } else {
+            alert("Vui lòng đăng nhập để thực hiện thao tác này");
+        }
+    };
+    return (
+        <section className="view">
+            <div className="container">
+                <h3 className="related-heading">Sản Phẩm Được Xem Nhiều</h3>
+                <div className="related-product">
+                    {products.map((item, i) => (
+                        <div className="products-item" key={i}>
+                            <div className="products-main">
+                                <div className="products-main-content">
+                                    <img
+                                        className="products-image"
+                                        src={item.image}
+                                        alt={item.name}
+                                    />
+                                    <div className="products-content">
+                                        <ul className="products-social">
+                                            <li className="products-social-item">
+                                                <Link
+                                                    to={`/product/${item.id_pd}`}
+                                                    className="products-social-link"
+                                                >
+                                                    <i className="fas fa-search"></i>
+                                                </Link>
+                                            </li>
+                                            <li className="products-social-item">
+                                                <span
+                                                    className="products-social-link cart"
+                                                    onClick={() =>
+                                                        handleAddToCart(item)
+                                                    }
+                                                >
+                                                    <i className="fas fa-cart-plus"></i>
+                                                </span>
+                                            </li>
+                                            <li className="products-social-item">
+                                                <span
+                                                    className="products-social-link"
+                                                    onClick={() =>
+                                                        HandleOnClickFavorite(
+                                                            item
+                                                        )
+                                                    }
+                                                >
+                                                    <i className="fas fa-heart"></i>
+                                                </span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="products-info">
+                                    <h4 className="products-name">
+                                        {item.name}
+                                    </h4>
+                                    <div className="products-color-list">
+                                        {colors[item.id_pd]?.map((color) => (
+                                            <div
+                                                key={color.id_color}
+                                                className={`products-color-item ${
+                                                    selectedColorIds[
+                                                        item.id_pd
+                                                    ] === color.id_color
+                                                        ? "active"
+                                                        : ""
+                                                }`}
+                                                style={{
+                                                    backgroundColor: color.code,
+                                                }}
+                                                onClick={() =>
+                                                    selectColor(
+                                                        item.id_pd,
+                                                        color.id_color
+                                                    )
+                                                }
+                                            ></div>
+                                        ))}
+                                    </div>
+                                    <div className="products-price">
+                                        <span>
+                                            {parseInt(
+                                                item.price_sale
+                                            ).toLocaleString("vi-VN", {
+                                                style: "currency",
+                                                currency: "VND",
+                                            })}
+                                        </span>
+                                        <del>
+                                            {parseInt(
+                                                item.price
+                                            ).toLocaleString("vi-VN", {
+                                                style: "currency",
+                                                currency: "VND",
+                                            })}
+                                        </del>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     ))}
-                  </div>
-                  <div className="products-price">
-                    <span>
-                      {parseInt(item.price_sale).toLocaleString('vi-VN', {
-                        style: 'currency',
-                        currency: 'VND',
-                      })}
-                    </span>
-                    <del>
-                      {parseInt(item.price).toLocaleString('vi-VN', {
-                        style: 'currency',
-                        currency: 'VND',
-                      })}
-                    </del>
-                  </div>
                 </div>
-              </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default ViewProduct;
