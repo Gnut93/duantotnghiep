5c996585781aa2240441de9190ba57aeb3cf1ea5
Ta Toan <tttoan1801@gmail.com>
2024-04-05 23:51:05 +0700

    fix add to card

--- Frontend/shopda/src/user/Components/BgHome/BgHome.js
+++ Frontend/shopda/src/user/Components/BgHome/BgHome.js
@@ -2,7 +2,20 @@ import React from 'react';
 import './BgHome.css';
 
 const BgHome = () => {
-  return <section className="bghome"></section>;
+  return (
+    <section className="bghome">
+      <div className="bghome-marquee">
+        <span className="bghome-title">
+          Hơn 40 Năm Kinh Nghiệm về Đồ
+          Da&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </span>
+        <span className="bghome-title">
+          Hơn 40 Năm Kinh Nghiệm về Đồ
+          Da&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </span>
+      </div>
+    </section>
+  );
 };
 
 export default BgHome;
