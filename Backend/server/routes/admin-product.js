9ce53f60f7cf6d718976c0e3be0f5421e85763b4
hoanls18302 <hoanlsps25637@fpt.edu.vn>
2024-04-13 20:28:16 +0700

    Sửa lại api product + api user

--- Backend/server/routes/admin-product.js
+++ Backend/server/routes/admin-product.js
@@ -92,118 +92,118 @@ router.delete("/delete/:id", async (req, res) => {
     }
 });
 
-//Thêm ảnh sản phẩm
-router.post("/add-image", async (req, res) => {
-    try {
-        const { name, id_pd } = req.body;
-        const sql = `INSERT INTO image (name,id_pd ) VALUES ( '${name}','${id_pd}')`;
-        await queryDB(sql);
-        res.json({ success: "Thêm hình sản phẩm thành công" });
-    } catch (err) {
-        res.status(403).json({ error: err.message });
-    }
-});
+// //Thêm ảnh sản phẩm
+// router.post("/add-image", async (req, res) => {
+//     try {
+//         const { name, id_pd } = req.body;
+//         const sql = `INSERT INTO image (name,id_pd ) VALUES ( '${name}','${id_pd}')`;
+//         await queryDB(sql);
+//         res.json({ success: "Thêm hình sản phẩm thành công" });
+//     } catch (err) {
+//         res.status(403).json({ error: err.message });
+//     }
+// });
 
-//Sửa ảnh sản phẩm
-router.put("/edit-image/:id", async (req, res) => {
-    var id_img = parseInt(req.params.id);
-    if (isNaN(id_img) || id_img < 1) {
-        res.json({ error: "ID không hợp lệ" });
-        return;
-    }
-    try {
-        const { name } = req.body;
-        const sql = `UPDATE image SET name='${name}' WHERE id_img='${id_img}'`;
-        await queryDB(sql);
-        res.json({ success: "Sửa hình sản phẩm thành công" });
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
+// //Sửa ảnh sản phẩm
+// router.put("/edit-image/:id", async (req, res) => {
+//     var id_img = parseInt(req.params.id);
+//     if (isNaN(id_img) || id_img < 1) {
+//         res.json({ error: "ID không hợp lệ" });
+//         return;
+//     }
+//     try {
+//         const { name } = req.body;
+//         const sql = `UPDATE image SET name='${name}' WHERE id_img='${id_img}'`;
+//         await queryDB(sql);
+//         res.json({ success: "Sửa hình sản phẩm thành công" });
+//     } catch (err) {
+//         res.json({ error: err.message });
+//     }
+// });
 
-//Xóa hình sản phẩm
-router.delete("/delete-image/:id", async (req, res) => {
-    var id_image = parseInt(req.params.id);
-    if (isNaN(id_image) || id_image < 1) {
-        res.json({ error: "ID không hợp lệ" });
-        return;
-    }
-    try {
-        await queryDB(`DELETE FROM image WHERE id_img='${id_image}'`);
-        res.json({ success: "Xóa hình sản phẩm thành công" });
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
+// //Xóa hình sản phẩm
+// router.delete("/delete-image/:id", async (req, res) => {
+//     var id_image = parseInt(req.params.id);
+//     if (isNaN(id_image) || id_image < 1) {
+//         res.json({ error: "ID không hợp lệ" });
+//         return;
+//     }
+//     try {
+//         await queryDB(`DELETE FROM image WHERE id_img='${id_image}'`);
+//         res.json({ success: "Xóa hình sản phẩm thành công" });
+//     } catch (err) {
+//         res.json({ error: err.message });
+//     }
+// });
 
-//Thêm màu sản phẩm
-router.post("/add-color", async (req, res) => {
-    try {
-        const { name, code, quantity, id_pd } = req.body;
-        const sql = `INSERT INTO color ( name, code, quantity,id_pd) VALUES ( '${name}', '${code}', '${quantity}','${id_pd}')`;
-        await queryDB(sql);
-        res.json({ success: "Thêm màu sản phẩm thành công" });
-    } catch (err) {
-        res.status(403).json({ error: err.message });
-    }
-});
+// //Thêm màu sản phẩm
+// router.post("/add-color", async (req, res) => {
+//     try {
+//         const { name, code, quantity, id_pd } = req.body;
+//         const sql = `INSERT INTO color ( name, code, quantity,id_pd) VALUES ( '${name}', '${code}', '${quantity}','${id_pd}')`;
+//         await queryDB(sql);
+//         res.json({ success: "Thêm màu sản phẩm thành công" });
+//     } catch (err) {
+//         res.status(403).json({ error: err.message });
+//     }
+// });
 
-//Sửa màu sản phẩm
-router.put("/edit-color/:id", async (req, res) => {
-    var id_color = parseInt(req.params.id);
-    if (isNaN(id_color) || id_color < 1) {
-        res.json({ error: "ID không hợp lệ" });
-        return;
-    }
-    try {
-        const { name, code, quantity } = req.body;
-        const sql = `UPDATE color SET name='${name}', code='${code}', quantity='${quantity}' WHERE id_color='${id_color}'`;
-        await queryDB(sql);
-        res.json({ success: "Sửa màu sản phẩm thành công" });
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
-// cập nhật số lượng sản phẩm
-router.put("/edit-quantity/:id", async (req, res) => {
-    var id_color = parseInt(req.params.id);
-    if (isNaN(id_color) || id_color < 1) {
-        res.json({ error: "ID không hợp lệ" });
-        return;
-    }
+// //Sửa màu sản phẩm
+// router.put("/edit-color/:id", async (req, res) => {
+//     var id_color = parseInt(req.params.id);
+//     if (isNaN(id_color) || id_color < 1) {
+//         res.json({ error: "ID không hợp lệ" });
+//         return;
+//     }
+//     try {
+//         const { name, code, quantity } = req.body;
+//         const sql = `UPDATE color SET name='${name}', code='${code}', quantity='${quantity}' WHERE id_color='${id_color}'`;
+//         await queryDB(sql);
+//         res.json({ success: "Sửa màu sản phẩm thành công" });
+//     } catch (err) {
+//         res.json({ error: err.message });
+//     }
+// });
+// // cập nhật số lượng sản phẩm
+// router.put("/edit-quantity/:id", async (req, res) => {
+//     var id_color = parseInt(req.params.id);
+//     if (isNaN(id_color) || id_color < 1) {
+//         res.json({ error: "ID không hợp lệ" });
+//         return;
+//     }
 
-    const updateTasks = []; // Mảng chứa các promise update
+//     const updateTasks = []; // Mảng chứa các promise update
 
-    try {
-        const updates = req.body; // Dữ liệu cập nhật [{ id_color: 50, quantity: 50 }, { id_color: 51, quantity: 50 }]
+//     try {
+//         const updates = req.body; // Dữ liệu cập nhật [{ id_color: 50, quantity: 50 }, { id_color: 51, quantity: 50 }]
 
-        updates.forEach(async (update) => {
-            const { id_color, quantity } = update;
-            const sql = `UPDATE color SET quantity='${quantity}' WHERE id_color='${id_color}'`;
-            updateTasks.push(queryDB(sql));
-        });
+//         updates.forEach(async (update) => {
+//             const { id_color, quantity } = update;
+//             const sql = `UPDATE color SET quantity='${quantity}' WHERE id_color='${id_color}'`;
+//             updateTasks.push(queryDB(sql));
+//         });
 
-        await Promise.all(updateTasks); // Chờ tất cả các promise update hoàn thành
+//         await Promise.all(updateTasks); // Chờ tất cả các promise update hoàn thành
 
-        res.json({ success: "Cập nhật số lượng sản phẩm thành công" });
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
+//         res.json({ success: "Cập nhật số lượng sản phẩm thành công" });
+//     } catch (err) {
+//         res.json({ error: err.message });
+//     }
+// });
 
-//Xóa màu sản phẩm
-router.delete("/delete-color/:id", async (req, res) => {
-    var id_color = parseInt(req.params.id);
-    if (isNaN(id_color) || id_color < 1) {
-        res.json({ error: "ID không hợp lệ" });
-        return;
-    }
-    try {
-        await queryDB(`DELETE FROM color WHERE id_color='${id_color}'`);
-        res.json({ success: "Xóa màu sản phẩm thành công" });
-    } catch (err) {
-        res.json({ error: err.message });
-    }
-});
+// //Xóa màu sản phẩm
+// router.delete("/delete-color/:id", async (req, res) => {
+//     var id_color = parseInt(req.params.id);
+//     if (isNaN(id_color) || id_color < 1) {
+//         res.json({ error: "ID không hợp lệ" });
+//         return;
+//     }
+//     try {
+//         await queryDB(`DELETE FROM color WHERE id_color='${id_color}'`);
+//         res.json({ success: "Xóa màu sản phẩm thành công" });
+//     } catch (err) {
+//         res.json({ error: err.message });
+//     }
+// });
 
 module.exports = router;
